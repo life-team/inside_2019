db7d0bacc4907101b59a3449a78bb17ed2579a62
madwayz <djurball@list.ru>
2019-03-16 00:48:28 +0700

    fix timeout

--- ppc/Eremin_PPC1/server/main.py
+++ ppc/Eremin_PPC1/server/main.py
@@ -19,7 +19,7 @@ def handle(connection, address):
                     pyatn[i][d] = k[random.randint(0, len(k) - 1)]
                     k.remove(pyatn[i][d])
             for i in range(4):
-                connection.sendall((str(pyatn[i][0]) + "\t" + str(pyatn[i][1]) + "\t" + str(pyatn[i][2]) + "\t" + str(pyatn[i][3]) + "\n").encode("utf-8"))
+                connection.send((str(pyatn[i][0]) + "\t" + str(pyatn[i][1]) + "\t" + str(pyatn[i][2]) + "\t" + str(pyatn[i][3]) + "\n").encode("utf-8"))
                 time.sleep(0.4)
             k = 0
             ryad = 0
